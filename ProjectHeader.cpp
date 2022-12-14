cab24bef5b40cf7da764200ecbfafe02f7da4736
AbdulMannan <fasitahir2019@gmail.com>
2022-12-14 23:57:50 +0500

    My First Few programs of C++

---
+++ ProjectHeader.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main(){
+ cout<< "**************************************"<<endl;
+ cout<< "*                                    *"<<endl;
+ cout<< "*            Library                 *"<<endl;
+ cout<< "*        Management System           *"<<endl;
+ cout<< "*                                    *"<<endl;
+ cout<< "*                                    *"<<endl; 
+ cout<< "**************************************"<<endl;
+}
